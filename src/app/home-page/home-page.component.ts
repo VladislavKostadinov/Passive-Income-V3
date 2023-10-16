fded38aa31e4aacd5a95ede86fb1dea7138b6484
VladislavKostadinov <vladicius@gmail.com>
2023-10-16 16:25:30 +0300

    Work on subscription
    Fixed some logic behind subs. Checks if user has already subbed with this e-mail.

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AngularFireDatabase } from '@angular/fire/compat/database';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-home-page',
@@ -20,10 +21,26 @@ faucetPay: string = '../../assets/images/faucetPay.webp';
 
 numberOfSubs = 0;
 email = "";
+emailDuplicate = false;
+currentSub = false;
+emailAction = false;
+listOfSubs:any = {};
 
 numberOfCmnts:number = 0;
-constructor(private db: AngularFireDatabase) {
-  
+constructor(private db: AngularFireDatabase, private snackBar: MatSnackBar) {
+  this.db.list('subsriptions').valueChanges().subscribe(
+    (data:any) => {
+      if (data) {
+        this.listOfSubs = data;
+        this.numberOfSubs = data.length
+        console.log("Change")
+        for (let x in data) {
+        }
+      } else {
+
+      }
+    }
+  );
 
   // this.db.list('minelab/comments').valueChanges().subscribe(
   //   (data) => {
@@ -39,16 +56,38 @@ constructor(private db: AngularFireDatabase) {
 }
 
 subsribe() {
-  this.db.list('subsriptions').valueChanges().subscribe(
-    (data) => {
-      this.numberOfSubs = data.length;
+  if (this.listOfSubs.length > 0) {
+    for (let sub in this.listOfSubs) {
+      if (this.listOfSubs[sub]['email'] == this.email) {
+        console.log((this.listOfSubs[sub]['email'] == this.email))
+        this.snackBar.open("You've already subscribed with this e-mail", "Dismiss")
+      } else {
+        this.addEmail()
+        this.snackBar.open("Thank you for subscribing", "Okay")
+      }
     }
-  )
-  console.log(this.email, this.numberOfSubs)
+  } else {
+    this.addEmail()
+    this.snackBar.open("Thank you for subscribing", "Okay")
+  }
+  
+}
+
+addEmail () {
   this.db.database.ref('subsriptions').child(this.numberOfSubs.toString()).set(
     {email: this.email}
+  ).catch(
+  (err) => {
+    if (err) {
+      this.snackBar.open("An error occurred. Sorry for the inconvenience.", "Dismiss")
+    }
+  }
   )
+}
 
+subsribeFailed () {
+  this.snackBar.open("You've already subscribed with this e-mail", "Dismiss")
+  
 }
 // test() {
 
